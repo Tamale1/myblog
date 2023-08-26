5043d9eac026899791008d510cfa9f4955c148e0
Tamale1 <yusufnkoyoyo@gmail.com>
2023-08-26 14:23:24 +0300

    more on client side

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,19 +1,75 @@
-import React from 'react'
+// import ReactQuill from "react-quill";
+// import 'react-quill/dist/quill.snow.css';
+
+import {useState} from "react";
+import {Navigate} from "react-router-dom";
+// import Editor from "../Editor";
 
 
 const CreatePost = () => {
+  const[title, setTitle]=useState('')
+  const[summary, setSummary]=useState('')
+  
+  const[content, setContent]=useState('')
+  const[files,setFiles]=useState('')
+  const [redirect, setRedirect] = useState(false);
+  // const modules= {
+  //   toolbar:[
+  //   [{ 'header': 1 }, { 'header': 2 }],
+  //   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  //   [{ 'script': 'sub'}, { 'script': 'super' }] ,     // superscript/subscript
+  //   [{ 'indent': '-1'}, { 'indent': '+1' }],         // outdent/indent
+  //   [{ 'direction': 'rtl' }], 
+  //   ]  
+  // }
+
+  async function createNewPost(e) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+    e.preventDefault();
+    const response = await fetch('http://localhost:9000/post', {
+      method: 'POST',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+ 
     return (
-        <form >
-          <input type="title" placeholder='title' />
+        <form onSubmit={createNewPost} >
+          <input type="title" placeholder={'title'}
+          value={title}
+          onChange={e=>setTitle(e.target.value)}
+          />
                
                  
-          <input type="summary"placeholder='Summary'  />
+        <input type="summary"placeholder={'Summary'} 
+         value={summary}
+         onChange={e=>setSummary(e.target.value)}
+        />
         
-          <input type="file" />
-        
+          <input type="file" value={files} onChange={e=>setFile(e.target.value)} />
+      
+      <textarea type= 'textarea' placeholder="add post"
+      value={content}
+      onChange={e=>setContent(e.target.value)}
+      ></textarea>
+         
+         
           <button style={{marginTop:'5px'}}>Create post</button>
         </form>
       );
-}
+  
+  }
+
 
 export default CreatePost
